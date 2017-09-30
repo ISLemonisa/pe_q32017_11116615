b5eb17a45e20e09fa69169123e35c41312cf59f3
ISLemonisa <lemon.isabella@yahoo.com.br>
2017-09-30 15:46:57 -0300

    adicionado ex 1

---
+++ lista01/01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int valor, resto;
+	printf("Insira um numero\n");
+	scanf("%d", &valor);
+	resto = valor%2;
+	if (resto == 0) {
+		printf("O numero e par\n");
+	}
+	else{
+		printf("O numero e impar\n");
+	}
+	return 0;
+
+}
