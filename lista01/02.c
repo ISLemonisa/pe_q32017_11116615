315148ff1a18c4d274ac16d9ef028053aabec9b2
ISLemonisa <lemon.isabella@yahoo.com.br>
2017-09-30 15:57:07 -0300

    ex2 added

---
+++ lista01/02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int x, y, z;
+	printf("Insira o valor de x:");
+	scanf("%d", &x);
+	printf("\nInsira o valor de y:");
+	scanf("%d", &y);
+	z = x + y;
+	printf("O resultado da soma é: %d", z);
+	return 0;
+}
